5f3edbbb8eda88e4cf6ade7d1f971c2bc01a2783
momolakeit <56810449+momolakeit@users.noreply.github.com>
2021-02-08 21:37:57 -0500

    retirer code inutile , monter coverage

--- src/main/java/com/momo/notaireApplication/service/ITextService.java
+++ src/main/java/com/momo/notaireApplication/service/ITextService.java
@@ -8,28 +8,5 @@ import java.io.*;
 
 @Service
 public class ITextService {
-    private String OWNER_PASS ="owner";
-    public byte[] encripterPdf(byte[] array){
-        try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            InputStream inputStream = new ByteArrayInputStream(array);
-            PdfReader reader =new PdfReader(inputStream);
-            PdfDocument pdfDoc = new PdfDocument(
-                    reader,
-                    new PdfWriter(outputStream, new WriterProperties().setStandardEncryption(
-                            // null user password argument is equal to empty string,
-                            // this means that no user password required
-                            null,
-                            OWNER_PASS.getBytes(),
-                            EncryptionConstants.ALLOW_PRINTING,
-                            EncryptionConstants.ENCRYPTION_AES_128 | EncryptionConstants.DO_NOT_ENCRYPT_METADATA))
-            );
-            pdfDoc.getWriter().write(array);
-            pdfDoc.close();
-            return outputStream.toByteArray();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+
 }
