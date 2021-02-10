d3f6e2125a6b2e1223b8bb903059d1b48f6b73b4
momolakeit <56810449+momolakeit@users.noreply.github.com>
2021-02-09 19:40:04 -0500

    retier import inutile

--- src/main/java/com/momo/notaireApplication/service/ITextService.java
+++ src/main/java/com/momo/notaireApplication/service/ITextService.java
@@ -1,10 +1,6 @@
 package com.momo.notaireApplication.service;
 
-import com.itextpdf.kernel.pdf.*;
-import com.momo.notaireApplication.utils.ApplicationFileUtils;
 import org.springframework.stereotype.Service;
-
-import java.io.*;
 
 @Service
 public class ITextService {
