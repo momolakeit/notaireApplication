9f80d65aa974503e0c1580f560d799d25a2caf16
momolakeit <56810449+momolakeit@users.noreply.github.com>
2021-02-21 09:39:01 -0500

    Update SecurityConfiguration.java

--- src/main/java/com/momo/notaireApplication/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/momo/notaireApplication/configuration/security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.momo.notaireApplication.configuration;
+package com.momo.notaireApplication.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
