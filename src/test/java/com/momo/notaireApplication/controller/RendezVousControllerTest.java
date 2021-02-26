af027a8edb1e149374ab2943e836568d72a0af9b
momolakeit <56810449+momolakeit@users.noreply.github.com>
2021-02-25 20:07:05 -0500

    terminer rendez vous controller test

--- src/test/java/com/momo/notaireApplication/controller/RendezVousControllerTest.java
+++ src/test/java/com/momo/notaireApplication/controller/RendezVousControllerTest.java
@@ -23,6 +23,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -48,8 +50,8 @@ public class RendezVousControllerTest {
 
     @BeforeEach
     public void init() {
-        RendezVous rendezVous = new RendezVous();
-        rendezVous.setLocalDateTime( LocalDateTime.ofInstant(Instant.ofEpochMilli(System.currentTimeMillis()), ZoneId.systemDefault()));
+        rendezVous = new RendezVous();
+        rendezVous.setLocalDateTime(LocalDateTime.ofInstant(Instant.ofEpochMilli(System.currentTimeMillis()), ZoneId.systemDefault()));
 
 
         client = userRepository.save(new Client());
@@ -62,11 +64,48 @@ public class RendezVousControllerTest {
         CreateRendezVousRequestDTO createRendezVousRequestDTO = initRequestDTO();
         MockMvc mvc = initMockMvc();
         mvc.perform(MockMvcRequestBuilders.post("/rendezVous/")
-                    .content(new ObjectMapper().writeValueAsString(createRendezVousRequestDTO))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(status().isOk());
+                .content(new ObjectMapper().writeValueAsString(createRendezVousRequestDTO))
+                .contentType(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
+
+    @Test
+    public void createRendezVousMauvaisePlageHoraireBadRequest() throws Exception {
+        client = initClientAvecRendezVousMauvaisePlageHoraire();
+        CreateRendezVousRequestDTO createRendezVousRequestDTO = initRequestDTO();
+        MockMvc mvc = initMockMvc();
+        mvc.perform(MockMvcRequestBuilders.post("/rendezVous/")
+                .content(new ObjectMapper().writeValueAsString(createRendezVousRequestDTO))
+                .contentType(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void fetchRendezVous() throws Exception {
+        MockMvc mvc = initMockMvc();
+        mvc.perform(MockMvcRequestBuilders.get("/rendezVous/getRendezVous/{id}", rendezVous.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+    @Test
+    public void fetchRendezVousNotFoundBadRequest() throws Exception {
+        MockMvc mvc = initMockMvc();
+        mvc.perform(MockMvcRequestBuilders.get("/rendezVous/getRendezVous/{id}", 0L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+
+    private Client initClientAvecRendezVousMauvaisePlageHoraire() {
+        client = new Client();
+        client.setRendezVous(new ArrayList<>(Arrays.asList(initRendezVousAvecMauvaisePlageHoraire())));
+        return userRepository.save(client);
+    }
+
 
     private CreateRendezVousRequestDTO initRequestDTO() {
         CreateRendezVousRequestDTO createRendezVousRequestDTO = new CreateRendezVousRequestDTO();
@@ -77,11 +116,11 @@ public class RendezVousControllerTest {
         return createRendezVousRequestDTO;
     }
 
-   /* private void initUsers(User user,int minutes){
+    private RendezVous initRendezVousAvecMauvaisePlageHoraire() {
         RendezVous rendezVous = new RendezVous();
-        rendezVous.setLocalDateTime();
-        userRepository.save(user);
-    }*/
+        rendezVous.setLocalDateTime(LocalDateTime.ofInstant(Instant.ofEpochMilli(System.currentTimeMillis()), ZoneId.systemDefault()));
+        return rendezVousRepository.save(rendezVous);
+    }
 
     private MockMvc initMockMvc() {
         return MockMvcBuilders.standaloneSetup(rendezVousController).build();
