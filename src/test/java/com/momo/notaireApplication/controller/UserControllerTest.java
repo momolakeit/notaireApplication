86237727b724cd963571e34696c224f2cea4d24a
momolakeit <56810449+momolakeit@users.noreply.github.com>
2021-03-14 09:42:51 -0400

    add search list controller test

--- src/test/java/com/momo/notaireApplication/controller/UserControllerTest.java
+++ src/test/java/com/momo/notaireApplication/controller/UserControllerTest.java
@@ -1,7 +1,10 @@
 package com.momo.notaireApplication.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.momo.notaireApplication.jwt.JwtProvider;
 import com.momo.notaireApplication.model.db.Notaire;
 import com.momo.notaireApplication.model.db.User;
+import com.momo.notaireApplication.model.request.UserSearchQuery;
 import com.momo.notaireApplication.repositories.UserRepository;
 import com.momo.notaireApplication.testUtils.ObjectTestUtils;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,13 +28,23 @@ class UserControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private User user;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    private String JWT_TOKEN;
 
     @BeforeEach
     public void init() {
         user = ObjectTestUtils.initNotaire();
         user.setId(null);
         user = userRepository.save(user);
+
+        JWT_TOKEN = jwtProvider.generate(user);
     }
 
 
@@ -72,6 +85,19 @@ class UserControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void fetchUserListByQuery() throws Exception {
+        MockMvc mvc = initMockMvc();
+        UserSearchQuery userSearchQuery = new UserSearchQuery();
+        userSearchQuery.setQuery("nom");
+        mvc.perform(MockMvcRequestBuilders.post("/user/search")
+                .content(objectMapper.writeValueAsString(userSearchQuery))
+                .header("Authorization", JWT_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     private MockMvc initMockMvc() {
