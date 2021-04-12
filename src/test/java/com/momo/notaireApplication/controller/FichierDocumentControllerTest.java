71cdd0cf926fc43c3e6b10961a5f3147bac4c925
momolakeit <56810449+momolakeit@users.noreply.github.com>
2021-04-11 21:31:26 -0400

    test du controller

--- src/test/java/com/momo/notaireApplication/controller/FichierDocumentControllerTest.java
+++ src/test/java/com/momo/notaireApplication/controller/FichierDocumentControllerTest.java
@@ -5,11 +5,9 @@ import com.momo.notaireApplication.mapping.UserMapper;
 import com.momo.notaireApplication.model.db.Client;
 import com.momo.notaireApplication.model.db.FichierDocument;
 import com.momo.notaireApplication.model.db.Notaire;
+import com.momo.notaireApplication.model.db.RendezVous;
 import com.momo.notaireApplication.model.request.CreateFichierDocumentRequestDTO;
-import com.momo.notaireApplication.repositories.ClientRepository;
-import com.momo.notaireApplication.repositories.FichierDocumentRepository;
-import com.momo.notaireApplication.repositories.NotaireRepository;
-import com.momo.notaireApplication.repositories.UserRepository;
+import com.momo.notaireApplication.repositories.*;
 import com.momo.notaireApplication.testUtils.TestDocumentUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -40,13 +38,10 @@ class FichierDocumentControllerTest {
     private FichierDocumentRepository fichierDocumentRepository;
 
     @Autowired
-    private NotaireRepository notaireRepository;
-
-    @Autowired
-    private ClientRepository clientRepository;
-
-    @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RendezVousRepository rendezVousRepository;
 
     private FichierDocument fichierDocument;
 
@@ -54,12 +49,15 @@ class FichierDocumentControllerTest {
 
     private Notaire notaire;
 
+    private RendezVous rendezVous;
+
     @BeforeEach
     public void init() {
         fichierDocument = new FichierDocument();
         fichierDocument = fichierDocumentRepository.save(fichierDocument);
         client = userRepository.save(new Client());
         notaire = userRepository.save(new Notaire());
+        rendezVous = rendezVousRepository.save(new RendezVous());
     }
 
     @Test
@@ -85,9 +83,9 @@ class FichierDocumentControllerTest {
     public void testCreateFichierDocument() throws Exception {
         MockMvc mvc = initMockMvc();
         CreateFichierDocumentRequestDTO createFichierDocumentRequestDTO = new CreateFichierDocumentRequestDTO();
-        createFichierDocumentRequestDTO.setClientId(UserMapper.instance.toDTO(client).getId());
-        createFichierDocumentRequestDTO.setNotaireId(UserMapper.instance.toDTO(notaire).getId());
-
+        createFichierDocumentRequestDTO.setClientId(client.getId());
+        createFichierDocumentRequestDTO.setNotaireId(notaire.getId());
+        createFichierDocumentRequestDTO.setRendezVousId(rendezVous.getId());
         mvc.perform(MockMvcRequestBuilders.post("/fichierDocument")
                 .content(new ObjectMapper().writeValueAsString(createFichierDocumentRequestDTO))
                 .contentType(MediaType.APPLICATION_JSON)
@@ -99,14 +97,13 @@ class FichierDocumentControllerTest {
     public void testUploadFichierDocumentData() throws Exception {
         MockMvc mvc = initMockMvc();
         MockMultipartFile file = new MockMultipartFile("file", "mysuperfile.pdf", "multipart/form-data", TestDocumentUtils.initPDFDocument());
-        mvc.perform(MockMvcRequestBuilders.multipart("/fichierDocument/upload/{documentId}",fichierDocument.getId())
+        mvc.perform(MockMvcRequestBuilders.multipart("/fichierDocument/upload/{documentId}", fichierDocument.getId())
                 .file(file)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
     }
-
 
 
     private MockMvc initMockMvc() {
